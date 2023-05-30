77bb151cab89e90ca594309d8fe7a58fdb19ec7d
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-05-30 21:25:49 +0300

    feat(alx-backend): add 1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const t = true; 
+    const t_2 = false;
+  }
+
+  return [t, t_2];
+}
